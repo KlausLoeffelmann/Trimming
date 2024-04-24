b173581a9c18d1c091d610699c93b58fe979553f
Klaus Loeffelmann <klaus.loeffelmann@microsoft.com>
2024-04-24 09:24:41 -0700

    Get rid of UnitOfWork approach for now.

--- src/cs/05Typical FullBlown LOB App/Net_9/TimeTamer/TimeTamer.Generic/UnitOfWork/IUnitOfWork.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TaskTamer.DataLayer.UnitOfWork;
-
-public interface IUnitOfWork : IDisposable
-{
-    Task<bool> SaveChangesAsync();
-    IRepository<T> Repository<T>() where T : class;
-}
